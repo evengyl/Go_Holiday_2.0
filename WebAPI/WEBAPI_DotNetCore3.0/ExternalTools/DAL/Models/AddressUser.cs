b9be36219a673104932282a476cf12b282452d22
evengyl <dark.evengyl@gmail.com>
2020-05-11 16:44:04 +0200

    add base project toolbox webAPI

---
+++ WebAPI/WEBAPI_DotNetCore3.0/ExternalTools/DAL/Models/AddressUser.cs
@@ -0,0 +1,11 @@
+﻿namespace DAL.Models
+{
+    public class AddressUser
+	{
+		public string Rue { get; set; }
+		public string Ville { get; set; }
+		public string Pays { get; set; }
+		public string Numero { get; set; }
+		public int CodePostal { get; set; }
+    }
+}
---
+++ WebAPI/WEBAPI_DotNetCore3.0/ExternalTools/DAL/Models/ComplementUser.cs
@@ -0,0 +1,11 @@
+﻿namespace DAL.Models
+{
+    public class ComplementUser
+    {
+        public string Gsm { get; set; }
+        public string Phone { get; set; }
+        public int Benefice { get; set; }
+        public int TotalPrice_InLocate { get; set; }
+        public int TotalPrice_InAttemps { get; set; }
+    }
+}
